9368b84ecda2ea00ed205b0885b4f2d404f12666
Maria Brown <mariabrown0908@gmail.com>
2022-05-31 14:49:49 -0400

    adding files

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I'm currently working on learning an array of technologies in the web development world.
+    
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Reach.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
+ 
+);
+
+export default Technologies;
---
+++ src/constants/constants.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: 'Future Pet Place',
+    description: "We designed Future Pet Place, a pet-finder application that helps users search for and locate available pets for adoption. This application was built using HTML, CSS, and Javascript languages. The JQuery and Materialize libraries were also used for this project.",
+      image: '/images/3.jpg',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    github: 'https://github.com/MariaEBrown/Future-Pet-Place',
+    visit: 'https://mariaebrown.github.io/Future-Pet-Place/',
+    id: 0,
+  },
+  {
+    title: 'Team Profile Generator',
+    description:"In this project my taks was to build a Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person. Jest was used for running the unit test and Inquirer for collecting input form the user.",
+    image: '/images/led-wall.jpg',
+    tags: ['Node js', 'JavaScript', '', 'Inquirer js', 'HTML'],
+    github: 'https://github.com/MariaEBrown/Team-Profile-Generator',
+    visit: 'https://mariaebrown.github.io/Team-Profile-Generator/',
+    id: 1,
+  },
+  {
+    title: 'Note Taker',
+    description: "This Note Taker App was creating to assist the user in keeping track of tasks to complete. This app was created using Express js to handle different HTTP methods in an API route. Also it utilized fetch API to make post requests.",
+      image: '/images/projects.jpeg',
+      tags: ['Express js','JavaScript', 'HTML', 'CSS'],
+    github: 'https://github.com/MariaEBrown/NoteApp',
+    visit: 'https://mariaebrown.github.io/NoteApp/',
+    id: 2,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
